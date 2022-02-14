e7610725d8ae23dd23c3c9a1588835a020b52f01
michaelschwob <michaelr@schwob.cc>
2022-02-14 16:15:10 -0600

    updated structure

--- exercises/Exercise 2/EX2.R
+++ exercises/Exercise 2/EX2.R
@@ -1,6 +1,11 @@
-## Gradient descent
+###
+### Fitting GLMs
+###
 
-# Data
+set.seed(702)
+setwd(paste0(getwd(), "/SDS383D-Schwob/data"))
+
+## Read and Structure Data
 data <- read.csv("wdbc.csv",header=FALSE)
 
 y <- as.numeric(data[,2]=="M")
@@ -16,49 +21,31 @@ X_test <- X[401:nrow(data),]
 
 p <- 11
 
-# log likelihood
+## Write log-likelihood function
 loglike <- function(y,theta){
   ll <-  sum(y*theta - log(1+exp(theta)))
   return(ll)
 }
 
-# Initialization
-beta_iter <- beta <- matrix(rep(0.1,11),ncol=1)
-ll_iter <- c()
-tol <- 1e-6
-gamma <- 0.2
-conv <- FALSE
-niter <- 0
-while (!conv){
-  g <- 0
-  for (i in 1:nrow(X_train)){
-    g <- g + (y_train[i] - exp(sum(X_train[i,]*beta))/(1+exp(sum(X_train[i,]*beta))))*X_train[i,]
-  }
-  g <- matrix(g,ncol=1)
-  beta <- beta + gamma*g
-  beta_iter <- cbind(beta_iter,beta)
-  theta <- X_train%*%beta
-  ll <- loglike(y_train,theta)
-  ll_iter <- c(ll_iter,ll)
-  if (abs(sum(g^2)) < tol ){
-    conv <- TRUE
-  }
-  niter <- niter + 1
-}
+###
+### Line Search Implementation
+###
 
-# Line search
-
+## Function to find optimal step (via line search)
 f_gamma <- function(gamma){
   theta <- X%*%(beta+gamma*g)
   ll <-  sum(y*theta - log(1+exp(theta)))
   return(-ll)
 }
 
+## Initializations
 beta_iter <- beta <- matrix(rep(0.1,11),ncol=1)
 ll_iter <- c()
 tol <- 1e-6
 conv <- FALSE
 niter <- 0
+
+## Gradient Descent Loop
 while (!conv){
   g <- 0
   for (i in 1:nrow(X_train)){
@@ -76,4 +63,3 @@ while (!conv){
   }
   niter <- niter + 1
 }
-
