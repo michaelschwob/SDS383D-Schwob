8c758d95a8b7a5dc34d25e8f073c76a397026728
michaelschwob <michaelr@schwob.cc>
2022-02-28 12:30:10 -0600

    add data source

---
+++ exercises/Exercise 3/GoGreen.R
@@ -0,0 +1,12 @@
+## title: Analyis of Green Buildings
+## author: Michael R. Schwob
+
+
+###
+### Problem Set-up
+###
+
+set.seed(702)
+setwd(paste0(getwd(), "/SDS383D-Schwob/data"))
+
+data <- read.csv("greenbuildings.csv")
