9c59bf75d3f3a5d8fc6f511d24d11fb79fd982de
michaelschwob <michaelr@schwob.cc>
2022-02-28 15:26:55 -0600

    add graphic and update code

--- exercises/Exercise 3/GoGreen.R
+++ exercises/Exercise 3/GoGreen.R
@@ -1,14 +1,18 @@
 ## title: Analyis of Green Buildings
 ## author: Michael R. Schwob
 
-
 ###
-### Problem Set-up
+### Set-up
 ###
 
 set.seed(702)
 setwd(paste0(getwd(), "/SDS383D-Schwob/data"))
-library(mvtnorm)
+library(mvtnorm) # sample from multivariate t distribution
+library(bayestestR) # calculate Bayesian credible intervals
+
+###
+### Structure Data
+###
 
 data <- read.csv("greenbuildings.csv")
 n <- dim(data)[1]
@@ -20,6 +24,10 @@ y <- data$Rent*data$leasing_rate/100
 X <- cbind(rep(1:n), data$green_rating, data$City_Market_Rent, data$age, data$class_a,  data$class_b)
 p <- dim(X)[2] # number of parameters
 
+###
+### Initializations
+###
+
 ## Priors
 K <- rep(0.001, p)
 K <- diag(K)
@@ -28,19 +36,35 @@ d <- 1
 eta <- 1
 Lambda <- diag(n)
 
-## Initialize Variables
+## Computations Using (c)
 nu.star <- n + d
 Lambda.star <- t(X)%*%Lambda%*%X + K
 mu.star <- solve(Lambda.star)%*%(t(X)%*%Lambda%*%y + t(K)%*%m)
-eta.star <- eta + t(y)%*%Lambda%*%y + t(m)%*%K%*%m - (t(y)%*%Lambda + t(m)%*%K)%*%X%*%solve(t(X)%*%Lambda%*%X+K)%*%t(t(y)%*%Lambda%*%X + t(m)%*%K)
-
-Sigma.star <- nu.star/eta.star*Lambda.star
-
-
-
+eta.star <- eta + t(y)%*%Lambda%*%y + t(m)%*%K%*%m - (t(y)%*%Lambda%*%X + t(m)%*%K)%*%solve(t(X)%*%Lambda%*%X + K)%*%t(t(y)%*%Lambda%*%X + t(m)%*%K)
+Sigma.star <- drop(eta.star/nu.star)*solve(Lambda.star)
 
 ###
-### Beta update
+### Beta updates
 ###
 
-beta.save[, i] <- rmvt(1, sigma = Lambda.star*nu.star/eta.star, df = nu.star, delta = mu.star)
+betas <- rmvt(n = 1000, sigma = Sigma.star, df = nu.star, delta = mu.star)
+
+###
+### Obtain 95% Intervals
+###
+
+## Using our method
+ci(betas[, 2], method = "HDI")
+
+## Using lm() method
+fit <- lm(y ~ 0 + X)
+confint(fit)[2, ]
+
+###
+### Residual Analysis
+###
+
+res <- y - X%*%apply(betas, 2, mean)
+png("hist.png")
+hist(res, main = "Histogram of Model Residuals", breaks = 50, col = "black", border = "black")
+dev.off()
