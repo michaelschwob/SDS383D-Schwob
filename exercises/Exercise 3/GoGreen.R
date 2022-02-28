90425ffd4c180ff22ee2e7c737e767c6bdad6393
michaelschwob <michaelr@schwob.cc>
2022-02-28 14:58:24 -0600

    clean file

--- exercises/Exercise 3/GoGreen.R
+++ exercises/Exercise 3/GoGreen.R
@@ -14,44 +14,33 @@ data <- read.csv("greenbuildings.csv")
 n <- dim(data)[1]
 
 ## Response  =  revenue/sq.ft.
-y <- data$Rent/data$leasing_rate*100
-y[which(data$leasing_rate == 0)] <- 0 # should we set to 0 or remove entirely ??
+y <- data$Rent*data$leasing_rate/100
 
 ## design matrix
 X <- cbind(rep(1:n), data$green_rating, data$City_Market_Rent, data$age, data$class_a,  data$class_b)
 p <- dim(X)[2] # number of parameters
 
-## Initialize Variables
+## Priors
+K <- rep(0.001, p)
+K <- diag(K)
+m <- rep(1, p)
+d <- 1
+eta <- 1
 Lambda <- diag(n)
-kappa <- c(.1, 1)
-K <- diag(p)
-for(i in 1:n){
-    if(i <= n/2){
-        K[i, i] <- kappa[1]
-    }else{
-        K[i, i] <- kappa[2]
-    }
-}
 
-## Gibbs Initialization
-M <- 10000
-beta.save <- matrix(0, p, M) # of parameters by # of Iterations
-d <- eta <- 1
+## Initialize Variables
 nu.star <- n + d
 Lambda.star <- t(X)%*%Lambda%*%X + K
-eta.star <- eta + t(y)%*%Lambda%*%y - t(y)%*%Lambda%*%X%*%solve(t(X)%*%Lambda%*%X+K)%*%t(t(y)%*%Lambda%*%X)
-mu.star <- solve(Lambda.star)%*%(t(X)%*%Lambda%*%y)
+mu.star <- solve(Lambda.star)%*%(t(X)%*%Lambda%*%y + t(K)%*%m)
+eta.star <- eta + t(y)%*%Lambda%*%y + t(m)%*%K%*%m - (t(y)%*%Lambda + t(m)%*%K)%*%X%*%solve(t(X)%*%Lambda%*%X+K)%*%t(t(y)%*%Lambda%*%X + t(m)%*%K)
+
+Sigma.star <- nu.star/eta.star*Lambda.star
+
+
+
 
 ###
-### Gibbs Sampler (to get beta)
+### Beta update
 ###
 
-for(i in 1:M){
-
-    ###
-    ### Sample beta
-    ###
-
-    beta.save[, i] <- rmvt(1, sigma = Lambda.star*nu.star/eta.star, df = nu.star, delta = mu.star)
-
-}
+beta.save[, i] <- rmvt(1, sigma = Lambda.star*nu.star/eta.star, df = nu.star, delta = mu.star)
