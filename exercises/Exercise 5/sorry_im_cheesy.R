4ed3d2c100931988a18a2f28dc8c062ccd690c51
michaelschwob <michaelr@schwob.cc>
2022-03-29 16:34:33 -0500

    added all updates

--- exercises/Exercise 5/sorry_im_cheesy.R
+++ exercises/Exercise 5/sorry_im_cheesy.R
@@ -62,11 +62,103 @@ a.save[1] <- a.0
 b.save[1] <- b.0
 sig2.save[, 1] <- s2i.0
 s2.save[, 1] <- sp2.0
+tmp.Sig <- diag(s2.save[, 1])
 mu.beta.save[, 1] <- mu.beta.0
 for(i in 1:n){
     beta.save[i, , 1] <- beta.i.0
 }  
 
+pb <- mrs.pb("Making that Chesse: ", n.mcmc)
+
 ###
 ### Gibbs Sampler
 ###
+
+for(k in 2:n.mcmc){
+
+    pb$tick()
+
+    ###
+    ### Update beta
+    ###
+    
+    for(i in 1:n){
+        Sig.star.inv <- t(X[1:(N.i[i]), , i])%*%X[1:(N.i[i]), , i]/sig2.save[i, k-1] + solve(tmp.Sig)
+        Sig.star <- solve(Sig.star.inv)
+
+        mu.star <- Sig.star%*%( t(X[1:(N.i[i]), , i])%*%Y[i, 1:(N.i[i])]/sig2.save[i, k-1] + solve(tmp.Sig)%*%mu.beta.save[, k-1]  ) # removed Y tranpose !!
+
+        beta.save[i, , k] <- rmvnorm(1, mu.star, Sig.star)
+    }
+    
+
+    ###
+    ### Update mu.beta
+    ###
+
+    mu.beta.save[, k] <- rmvnorm(1, rep(mean(beta.save[, , k]), 4), tmp.Sig/n) # does this seem right??
+
+    ###
+    ### Update s2.p
+    ###
+
+    for(p in 1:4){
+        tmp.a <- n/2 + 1/2
+        tmp.sum <- 0
+        for(i in 1:n){
+            tmp.sum <- tmp.sum + (beta.save[i, p, k] - mu.beta.save[p, k])^2 # tranposed beta.save
+        }
+        tmp.b <- 1/2*(1 + tmp.sum)
+        s2.save[p, k] <- rinvgamma(1, tmp.a, tmp.b)
+    }
+    tmp.Sig <- diag(s2.save[, k])
+
+    ###
+    ### Update sigma2.i
+    ###
+
+    for(i in 1:n){
+        tmp.a <- a.save[k-1]/2 + 1/2
+        tmp.mat <- matrix(Y[i, 1:N.i[i]]) - X[1:N.i[i], , i]%*%beta.save[i, , k]
+        tmp.b <- 1/2*(b.save[k-1] + t(tmp.mat)%*%tmp.mat) # tranposed beta and Y
+
+        sig2.save[i, k] <- rinvgamma(1, tmp.a, tmp.b)
+    }
+
+    ###
+    ### Update a
+    ###
+
+    a.star <- runif(1, 1, 10)
+
+    mh1 <- sum(dinvgamma(sig2.save[, k], a.star/2, b.save[k-1]/2, log = TRUE)) + dgamma(a.star, 3, 1, log = TRUE) + dunif(a.save[k-1], 0, 10, log = TRUE)
+    mh2 <- sum(dinvgamma(sig2.save[, k], a.save[k-1]/2, b.save[k-1]/2, log = TRUE)) + dgamma(a.save[k-1], 3, 1, log = TRUE) + dunif(a.star, 0, 10, log = TRUE)
+    mh <- exp(mh1 - mh2)
+    if(mh > runif(1)){
+        a.save[k] <- a.star # use proposed value
+    }else{
+        a.save[k] <- a.save[k-1] # retain value
+    }
+
+    ### 
+    ### Update b
+    ###
+
+    b.star <- runif(1, 1, 10)
+
+    mh1 <- sum(dinvgamma(sig2.save[, k], a.save[k]/2, b.star/2, log = TRUE)) + dgamma(b.star, 3, 1, log = TRUE) + dunif(b.save[k-1], 0, 10, log = TRUE)
+    mh2 <- sum(dinvgamma(sig2.save[, k], a.save[k]/2, b.save[k-1]/2, log = TRUE)) + dgamma(b.save[k-1], 3, 1, log = TRUE) + dunif(b.star, 0, 10, log = TRUE)
+    mh <- exp(mh1 - mh2)
+    if(mh > runif(1)){
+        b.save[k] <- b.star # use proposed value
+    }else{
+        b.save[k] <- b.save[k-1] # retain value
+    }
+
+}
+
+###
+### Inference
+###
+
+sig2.df <- data.frame()
