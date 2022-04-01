aa2672d0d0162329bca9dcbff591ab762ce383cc
michaelschwob <michaelr@schwob.cc>
2022-04-01 11:36:27 -0500

    add plots

--- exercises/Exercise 5/natewho.R
+++ exercises/Exercise 5/natewho.R
@@ -35,7 +35,7 @@ names <- unique(data$state)
 n <- length(names)
 biggest.sample <- names(which.max(table(data$state)))
 max <- sum(data$state == biggest.sample) # largest number of observations per store
-P <- 10
+P <- 8 # was 10
 
 Y <- matrix(NA, n, max)
 X <- array(NA, dim = c(max, P+1, n))
@@ -46,7 +46,7 @@ for(i in 1:n){
     N.i[i] <- dim(tmp.data)[1] # number of observations for store i
     Y[i, 1:(N.i[i])] <- tmp.data$bush # states voters
     for(j in 1:N.i[i]){
-        X[j, , i] <- c(1, as.numeric(tmp.data[j, 3:12]))
+        X[j, , i] <- c(1, as.numeric(tmp.data[j, c(3, 4, 5, 6, 7, 9, 10, 11)])) # was 3:12
     }
 }
 
@@ -67,3 +67,108 @@ for(i in 1:n){
     }
 }
 
+nu.0 <- P + 2 # check if this is okay
+
+###
+### Save Matrices
+###
+
+n.mcmc <- 10000
+beta.save <- array(NA, dim = c(P+1, n, n.mcmc))
+alpha.save <- array(NA, dim = c(P+1, n, n.mcmc))
+Z.save <- array(NA, dim = c(n, max, n.mcmc))
+
+beta.save[, , 1] <- beta.star
+alpha.save[, , 1] <- alpha
+Z.save[, , 1] <- Z
+
+pb <- mrs.pb("Nate Silver has nothing on this: ", n.mcmc)
+
+###
+### MCMC
+###
+
+for(k in 2:n.mcmc){
+
+    pb$tick()
+
+    ###
+    ### Update alpha
+    ###
+
+    for(i in 1:n){
+        B.tilde <- solve(solve(B.star) + t(X[1:N.i[i], , i])%*%X[1:N.i[i], , i])
+        beta.tilde <- B.tilde%*%(  solve(B.star)%*%beta.save[, i, k-1] +  t(X[1:N.i[i], , i])%*%Z.save[i, 1:N.i[i], k-1] )
+
+        alpha.save[, i, k] <- rmvnorm(1, beta.tilde, B.tilde, checkSymmetry = FALSE) # added last argument
+    }
+
+    ###
+    ### Update Z
+    ###
+
+    for(i in 1:n){
+        for(j in 1:N.i[i]){
+            if(Y[i, j] == 1){
+                Z.save[i, j, k] <- rtruncnorm(1, a = 0, mean = X[j, , i]%*%alpha.save[, i, k], sd = 1)
+            }
+            if(Y[i, j] == 0){
+                Z.save[i, j, k] <- rtruncnorm(1, b = 0, mean = X[j, , i]%*%alpha.save[, i, k], sd = 1)
+            }
+        }
+    }
+
+    ###
+    ### Update beta.star
+    ###
+
+    for(i in 1:n){
+        A.inv <- solve( solve(B.star) + solve(10^4*diag(P+1))  ) 
+        tmp.mean <- A.inv%*%( solve(B.star)%*%alpha.save[, i, k] )
+        beta.save[, i, k] <- rmvnorm(1, tmp.mean, A.inv)
+    }
+
+    ###
+    ### Update B.star
+    ###
+
+    nu <- n + nu.0
+    tmp.sum <- 0
+    for(i in 1:n){
+        tmp.sum <- tmp.sum + (alpha.save[, i, k] - beta.save[, i, k])%*%t(alpha.save[, i, k] - beta.save[, i, k])
+    }
+    S <- diag(P + 1) + tmp.sum
+
+    B.star <- rinvwishart(nu, S)
+
+}
+
+###
+### Trace Plots for alphas for each state
+###
+
+n.burn <- .3*n.mcmc
+for(i in 1:n){
+    tmp.df <- data.frame(iter = n.burn:n.mcmc, mu = alpha.save[1, i, n.burn:n.mcmc], beta1 = alpha.save[2, i, n.burn:n.mcmc], beta2 = alpha.save[3, i, n.burn:n.mcmc], beta3 = alpha.save[4, i, n.burn:n.mcmc], beta4 = alpha.save[5, i, n.burn:n.mcmc], beta5 = alpha.save[6, i, n.burn:n.mcmc], beta6 = alpha.save[7, i, n.burn:n.mcmc], beta7 = alpha.save[8, i, n.burn:n.mcmc], beta8 = alpha.save[9, i, n.burn:n.mcmc])
+    plot.df <- tmp.df %>% gather(key = "variable", value = "value", -1)
+    traces <- ggplot(plot.df, aes(x = iter, y = value)) + geom_line(aes(color = variable)) + theme_classic() + ggtitle("Trace Plots") + xlab("Iteration") + ylab("Value")
+    assign(paste0("t", i), traces)
+}
+
+#t2
+#t4
+#t8
+#t9
+#t14
+
+### Compare t2 (good) and t3 (bad)
+N.i[2] # more observations
+N.i[3] # less observations
+diff <- X[,,2]-X[,,3]
+X2 <- as.matrix(X[1:N.i[2], , 2])
+X3 <- X[1:N.i[3], , 3]
+
+your.matrix <- X3
+
+rankifremoved <- sapply(1:ncol(your.matrix), function (x) qr(your.matrix[,-x])$rank)
+which(rankifremoved == max(rankifremoved))
