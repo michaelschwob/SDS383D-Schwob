1c95d9163533489bf3bcbc02645afb98382fa46f
michaelschwob <michaelr@schwob.cc>
2022-04-02 10:27:15 -0500

    update graphic

--- exercises/Exercise 6/validate_me.R
+++ exercises/Exercise 6/validate_me.R
@@ -15,19 +15,25 @@ setwd(paste0(getwd(), "/SDS383D-Schwob/exercises/Exercise 6"))
 source("smooth_like_Jagger.R")
 
 X.1 <- rnorm(700, 0, 40) # training data
-X.2 <- rnorm(300, 0, 20) # testing data; must be within range of X.1
+#X.1 <- scale(X.1)
+#X.2 <- rnorm(300, 0, 20) # testing data; must be within range of X.1
+X.2 <- sample(X.1, 300, replace = TRUE) # hear me out !!
 X <- c(X.1, X.2) # combine training and testing data
-Y <- f.func(X)
-data <- cbind(scale(Y), scale(X))
+noise <- rnorm(1000, 0, 3)
+Y <- f.func(X) + noise
+data <- cbind(Y, X)
 train.data <- data[1:700, ]
 test.data <- data[701:1000, ]
 
 ##############################################################
 
-H <- c(1, 2, 5, 1)
+H <- c(1, 2, 5, 10)
 M <- 1000
-x.grid <- seq(-10, 10, length.out = M)
+x.grid <- seq(-100, 100, length.out = M)
 MSPE <- rep(0, length(H))
+
+Y.matrix <- matrix(0, M, length(H) + 1)
+Y.matrix[, 1] <- f.func(x.grid)
 
 pb <- mrs.pb("I need more validation. ", length(H))
 
@@ -48,7 +54,7 @@ for(k in 1:length(H)){
     }
 
     ## Approximate Function
-    approx.func <- approxfun(x.grid, smooth.y)
+    approx.func <- approxfun(x.grid, smooth.y, rule = 2) # rule = 2 is needed to extrapolate
 
     ## Obtain MSPE from test data
     MSPE.tmp <- 0
@@ -56,6 +62,14 @@ for(k in 1:length(H)){
         MSPE.tmp <- MSPE.tmp + (test.data[i, 1] - approx.func(test.data[i, 2]))^2
     }
     MSPE[k] <- MSPE.tmp/dim(test.data)[1]
-    
+
+    Y.matrix[, k + 1] <- smooth.y
 }
-plot(x.grid,smooth.y)
+
+colnames(Y.matrix) <- c("true", "h = 1", "h = 2", "h = 5", "h = 10")
+plot.df <- data.frame(cbind(x.grid, Y.matrix[, -1]))
+plot.df <- plot.df %>% gather(key = "h", value = "value", -1)
+point.df <- data.frame(test.y = Y[701:1000], test.x = X.2)
+
+plot <- ggplot(plot.df, aes(x = x.grid, y = value)) + geom_line(aes(color = h)) + theme_classic() + ggtitle("Cross Validation with Different Bandwidths h") + xlab("x") + ylab("Estimated Value") + geom_point(point.df, mapping = aes(x = test.x, y = test.y), alpha = 0.6)
+ggsave("cross_validation.png", plot)
