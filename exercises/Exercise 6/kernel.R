56c990e3a572d4e5f1bd97b79eaf3ed87496c382
michaelschwob <michaelr@schwob.cc>
2022-04-01 20:00:52 -0500

    cleaned code

--- exercises/Exercise 6/kernel.R
+++ exercises/Exercise 6/kernel.R
@@ -18,11 +18,6 @@ f.func <- function(x){
 }
 
 ## Weighting Functions
-weight.func <- function(x.vec, new.x, h){
-    int <- (x.vec - new.x)/h
-    weights <- gauss.kernel(int)/h
-    return(weights)
-}
 weight.ind <- function(x.old, x.new, h){
     int <- (x.old - x.new)/h
     weight <- gauss.kernel(int)/h
@@ -46,7 +41,11 @@ x.grid <- seq(-10, 10, length.out = M)
 Y.matrix <- matrix(0, M, length(H) + 1)
 Y.matrix[, 1] <- f.func(x.grid)
 
+pb <- mrs.pb("Smooth like Jagger ", length(H))
+
 for(k in 1:length(H)){
+
+    pb$tick()
 
     h <- H[k]
     smooth.y <- rep(0, M)
@@ -64,7 +63,6 @@ for(k in 1:length(H)){
 colnames(Y.matrix) <- c("true", "h = 1", "h = 2", "h = 5", "h = 10")
 plot.df <- data.frame(cbind(x.grid, Y.matrix[, -1]))
 plot.df <- plot.df %>% gather(key = "h", value = "value", -1)
-
 
 plot <- ggplot(plot.df, aes(x = x.grid, y = value)) + geom_line(aes(color = h)) + theme_classic() + ggtitle("Estimated Functions with Various h") + xlab("x") + ylab("Estimated Value")
 ggsave("kernel.png", plot)
